cab4f562d28ea4e10757e7491f6b227d4ddccb34
SimonCuany <47849679+SimonCuany@users.noreply.github.com>
2019-09-30 15:59:36 +0200

    exo div

---
+++ Exercice/JavaScript/logique.js
@@ -0,0 +1,24 @@
+div1.addEventListener('click', makeblue)
+
+function makeblue() {
+    div1.classList.add("makeblue")
+}
+
+div2.addEventListener('click', remove)
+
+function remove() {
+    div1.classList.remove('makeblue')
+}
+
+div3.addEventListener('mouseover')
+
+function arial() {
+div3.style.fontFamily="arial"
+
+}
+
+
+
+
+
+
