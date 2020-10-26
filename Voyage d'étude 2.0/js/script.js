1be0388aa6b48f8fd5f18b1703457de46ee1cd70
Simon Cuany <47849679+SimonCuany@users.noreply.github.com>
2020-10-26 11:16:48 +0100

    Update Git repository
    I've found a bug and i resolve it today

---
+++ Voyage d'étude 2.0/js/script.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', function () {
+//Afficher les participants
+    document.getElementById("Btn_Participants").addEventListener("click", show_participant);
+
+    function show_participant() {
+        document.getElementById('Participants').classList.remove('hidden')
+    }
+
+
+//Afficher les Transports
+    document.getElementById("Btn_Transports").addEventListener("click", show_transport);
+
+    function show_transport() {
+        document.getElementById('Transports').classList.remove('hidden')
+        document.getElementById('Participants').classList.add('hidden')
+        document.getElementById('Hebergement').classList.add('hidden')
+        document.getElementById('Activités').classList.add('hidden')
+    }
+
+//Afficher les hébergements
+    document.getElementById("Btn_Hebergement").addEventListener("click", show_hebergement);
+
+    function show_hebergement() {
+        document.getElementById('Hebergement').classList.remove('hidden')
+        document.getElementById('Participants').classList.add('hidden')
+        document.getElementById('Transports').classList.add('hidden')
+        document.getElementById('Activités').classList.add('hidden')
+    }
+
+//Afficher les activités
+    document.getElementById("Btn_Activités").addEventListener("click", show_activite);
+
+    function show_activite() {
+        document.getElementById('Activités').classList.remove('hidden')
+        document.getElementById('Transports').classList.add('hidden')
+        document.getElementById('Hebergement').classList.add('hidden')
+        document.getElementById('Participants').classList.add('hidden')
+    }
+})
